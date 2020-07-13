7899b390d132972c24e70ab20813b8495b3f675d
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-07-13 13:22:09 -0500

    Cleaned up JEI compat

--- src/main/java/com/team_abnormals/environmental/integration/jei/EnvironmentalJEIPlugin.java
+++ src/main/java/com/team_abnormals/environmental/integration/jei/EnvironmentalJEIPlugin.java
@@ -1,24 +1,38 @@
 package com.team_abnormals.environmental.integration.jei;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Nullable;
 
 import com.team_abnormals.environmental.client.gui.screen.inventory.KilnScreen;
 import com.team_abnormals.environmental.common.inventory.container.KilnContainer;
+import com.team_abnormals.environmental.common.item.crafting.BakingRecipe;
 import com.team_abnormals.environmental.core.Environmental;
 import com.team_abnormals.environmental.core.registry.EnvironmentalBlocks;
+import com.team_abnormals.environmental.core.registry.EnvironmentalRecipes;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.helpers.IJeiHelpers;
+import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.IRecipeTransferRegistration;
 import mezz.jei.util.ErrorUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.util.ResourceLocation;
 
 @JeiPlugin
@@ -44,7 +58,7 @@ public class EnvironmentalJEIPlugin implements IModPlugin {
 	public void registerRecipes(IRecipeRegistration registration) {
 		ErrorUtil.checkNotNull(bakingCategory, "bakingCategory");
 
-		EnvironmentalRecipeValidator.Results recipes = EnvironmentalRecipeValidator.getValidRecipes(bakingCategory);
+		Results recipes = getValidRecipes(bakingCategory);
 		registration.addRecipes(recipes.getBakingRecipes(), BakingCategory.BAKING);
 	}
 
@@ -61,6 +75,38 @@ public class EnvironmentalJEIPlugin implements IModPlugin {
 	@Override
 	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
 		registration.addRecipeCatalyst(new ItemStack(EnvironmentalBlocks.KILN.get()), BakingCategory.BAKING, VanillaRecipeCategoryUid.FUEL);
+	}
+	
+	public static class Results {
+		private final List<BakingRecipe> bakingRecipes = new ArrayList<>();
+		private final List<BakingRecipe> sawingRecipes = new ArrayList<>();
+
+		public List<BakingRecipe> getBakingRecipes() {
+			return bakingRecipes;
+		}
+		
+		public List<BakingRecipe> getSawingRecipes() {
+			return sawingRecipes;
+		}
+	}
+
+	public static Results getValidRecipes(IRecipeCategory<BakingRecipe> bakingCategory) {
+
+		Results results = new Results();
+		ClientWorld world = Minecraft.getInstance().world;
+		RecipeManager recipeManager = world.getRecipeManager();
+
+		for (BakingRecipe recipe : getRecipes(recipeManager, EnvironmentalRecipes.RecipeTypes.BAKING)) {
+			results.bakingRecipes.add(recipe);
+		}
+
+		return results;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <C extends IInventory, T extends IRecipe<C>> Collection<T> getRecipes(RecipeManager recipeManager, IRecipeType<T> recipeType) {
+		Map<ResourceLocation, IRecipe<C>> recipesMap = recipeManager.getRecipes(recipeType);
+		return (Collection<T>) recipesMap.values();
 	}
 
 }
