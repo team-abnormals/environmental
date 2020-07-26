c715011dbc5eeb90c5d4b17e3f7789c85a594800
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-07-26 10:16:52 -0500

    the imports jackson

--- src/main/java/com/team_abnormals/environmental/common/item/ExecutionerCleaverItem.java
+++ src/main/java/com/team_abnormals/environmental/common/item/ExecutionerCleaverItem.java
@@ -2,26 +2,13 @@ package com.team_abnormals.environmental.common.item;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import com.team_abnormals.environmental.core.Environmental;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.SwordItem;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.World;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 public class ExecutionerCleaverItem extends SwordItem {
     private final float attackDamage;
