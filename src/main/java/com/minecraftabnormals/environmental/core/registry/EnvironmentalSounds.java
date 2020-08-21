36f5b4bb0df98cd6338b8cd840f74f646da5b21a
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-08-21 12:21:18 -0500

    Shingle replacement recipes

--- src/main/java/com/minecraftabnormals/environmental/core/registry/EnvironmentalSounds.java
+++ src/main/java/com/minecraftabnormals/environmental/core/registry/EnvironmentalSounds.java
@@ -11,15 +11,15 @@ public class EnvironmentalSounds {
 
     public static final RegistryHelper HELPER = Environmental.REGISTRY_HELPER;
 
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_BURP = HELPER.createSoundEvent("entity.slabfish.burp");
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_DEATH = HELPER.createSoundEvent("entity.slabfish.death");
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_STEP = HELPER.createSoundEvent("entity.slabfish.step");
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_HURT = HELPER.createSoundEvent("entity.slabfish.hurt");
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_EAT = HELPER.createSoundEvent("entity.slabfish.eat");
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_TRANSFORM = HELPER.createSoundEvent("entity.slabfish.transform");
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_BACKPACK = HELPER.createSoundEvent("entity.slabfish.backpack");
-    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_SWEATER = HELPER.createSoundEvent("entity.slabfish.sweater");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_BURP         = HELPER.createSoundEvent("entity.slabfish.burp");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_DEATH        = HELPER.createSoundEvent("entity.slabfish.death");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_STEP         = HELPER.createSoundEvent("entity.slabfish.step");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_HURT         = HELPER.createSoundEvent("entity.slabfish.hurt");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_EAT          = HELPER.createSoundEvent("entity.slabfish.eat");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_TRANSFORM    = HELPER.createSoundEvent("entity.slabfish.transform");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_BACKPACK     = HELPER.createSoundEvent("entity.slabfish.backpack");
+    public static final RegistryObject<SoundEvent> ENTITY_SLABFISH_SWEATER      = HELPER.createSoundEvent("entity.slabfish.sweater");
 
-    public static final RegistryObject<SoundEvent> SLABRAVE = HELPER.createSoundEvent("music.record.slabrave");
+    public static final RegistryObject<SoundEvent> SLABRAVE     = HELPER.createSoundEvent("music.record.slabrave");
     public static final RegistryObject<SoundEvent> LEAVING_HOME = HELPER.createSoundEvent("music.record.leaving_home");
 }
