c1591c05e4cc37aef24122e69eb0bbeb4440fe12
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-08-26 01:20:13 -0500

    Carpenter Trades

--- src/main/java/com/minecraftabnormals/environmental/core/other/EnvironmentalTrades.java
+++ src/main/java/com/minecraftabnormals/environmental/core/other/EnvironmentalTrades.java
@@ -1,13 +1,19 @@
 package com.minecraftabnormals.environmental.core.other;
 
+import java.util.List;
+
 import com.minecraftabnormals.environmental.core.Environmental;
 import com.minecraftabnormals.environmental.core.registry.EnvironmentalBlocks;
 import com.minecraftabnormals.environmental.core.registry.EnvironmentalItems;
 import com.minecraftabnormals.environmental.core.registry.EnvironmentalVillagers;
 import com.teamabnormals.abnormals_core.core.utils.TradeUtils;
 import com.teamabnormals.abnormals_core.core.utils.TradeUtils.ItemsForEmeraldsTrade;
+
 import net.minecraft.entity.merchant.villager.VillagerProfession;
+import net.minecraft.entity.merchant.villager.VillagerTrades.ITrade;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.tags.ItemTags;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -51,15 +57,75 @@ public class EnvironmentalTrades {
 	
 	@SubscribeEvent
 	public static void onCarpenterTradesEvent(VillagerTradesEvent event) {
+		String[] defaultRemoved = new String[] {"crimson", "warped", "poise"};
+		List<ITrade> novice = event.getTrades().get(1);
+		List<ITrade> apprentice = event.getTrades().get(2);
+		List<ITrade> journeyman = event.getTrades().get(3);
+		List<ITrade> expert = event.getTrades().get(4);
+		List<ITrade> master = event.getTrades().get(5);
+
 		if(event.getType() == EnvironmentalVillagers.CARPENTER.get()) {
-			event.getTrades().get(1).add(new TradeUtils.EmeraldsForItemsTrade(Items.ACACIA_BOAT, 23, 1, 6, 1));
+			
+			// NOVICE //			
+			for(Item item : ItemTags.PLANKS.getAllElements()) {
+				if (notOnBlacklist(item, new String[] {"vertical", "crimson", "warped", "poise"})) {
+					novice.add(new TradeUtils.EmeraldsForItemsTrade(item, 24, 1, 16, 3));
+				}
+			}
+			
+			for(Item item : ItemTags.field_232912_o_.getAllElements()) {
+				if (notOnBlacklist(item, new String[] {"stripped", "_wood"})) {
+					novice.add(new TradeUtils.EmeraldsForItemsTrade(item, 6, 1, 16, 4));
+				}
+			}
+			
+			// APPRENTICE //
+			for(Item item : ItemTags.SIGNS.getAllElements()) {
+				if (notOnBlacklist(item, defaultRemoved)) {
+					apprentice.add(new TradeUtils.EmeraldsForItemsTrade(item, 6, 1, 4, 10));
+				}
+			}
+			
+			for(Item item : ItemTags.WOODEN_TRAPDOORS.getAllElements()) {
+				if (notOnBlacklist(item, defaultRemoved)) {
+					apprentice.add(new TradeUtils.ItemsForEmeraldsTrade(item, 1, 6, 4, 10));
+				}
+			}
+			
+			// JOURNEYMAN //
+			for(Item item : ItemTags.SAPLINGS.getAllElements()) {
+				journeyman.add(new TradeUtils.EmeraldsForItemsTrade(item, 8, 1, 16, 10));
+			}
+			
+			for(Item item : ItemTags.WOODEN_DOORS.getAllElements()) {
+				if (notOnBlacklist(item, defaultRemoved)) {
+					journeyman.add(new TradeUtils.ItemsForEmeraldsTrade(item, 1, 3, 5, 15));
+				}
+			}
+			
+			// EXPERT //
+			expert.add(new TradeUtils.EmeraldsForItemsTrade(Items.STICK, 23, 1, 16, 3));
+			expert.add(new TradeUtils.EmeraldsForItemsTrade(Items.IRON_AXE, 1, 6, 8, 10));
+			expert.add(new TradeUtils.EmeraldsForItemsTrade(Items.GOLDEN_AXE, 1, 7, 1, 20));
+
+			// MASTER //
+			master.add(new TradeUtils.ItemsForEmeraldsTrade(EnvironmentalItems.CONSTRUCTOR_BELT.get(), 24, 1, 4, 5));
 		}
+	}
+	
+	private static boolean notOnBlacklist(Item item, String[] items) {
+		for (String name : items) {
+			if (item.getRegistryName().toString().contains(name)) return false;
+		}
+		return true;
 	}
 	
 	@SubscribeEvent
 	public static void onCeramistTradesEvent(VillagerTradesEvent event) {
+		List<ITrade> novice = event.getTrades().get(1);
 		if(event.getType() == EnvironmentalVillagers.CERAMIST.get()) {
-			event.getTrades().get(1).add(new TradeUtils.EmeraldsForItemsTrade(Items.ACACIA_BOAT, 23, 1, 6, 1));
+			novice.add(new TradeUtils.EmeraldsForItemsTrade(Items.ACACIA_BOAT, 23, 1, 6, 1));
+			
 		}
 	}
 	
