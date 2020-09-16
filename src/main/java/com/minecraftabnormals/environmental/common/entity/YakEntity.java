275466568b6d416791c5844a3b801749999a1afe
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-09-16 00:05:36 -0500

    Tried to make yaks attack

--- src/main/java/com/minecraftabnormals/environmental/common/entity/YakEntity.java
+++ src/main/java/com/minecraftabnormals/environmental/common/entity/YakEntity.java
@@ -1,10 +1,12 @@
 package com.minecraftabnormals.environmental.common.entity;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.minecraftabnormals.environmental.common.item.YakPantsItem;
 import com.minecraftabnormals.environmental.core.registry.EnvironmentalEntities;
 import com.minecraftabnormals.environmental.core.registry.EnvironmentalItems;
 
@@ -12,22 +14,30 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntitySize;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.IAngerable;
 import net.minecraft.entity.IShearable;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.Pose;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.entity.ai.goal.BreedGoal;
 import net.minecraft.entity.ai.goal.EatGrassGoal;
 import net.minecraft.entity.ai.goal.FollowParentGoal;
 import net.minecraft.entity.ai.goal.HurtByTargetGoal;
 import net.minecraft.entity.ai.goal.LookAtGoal;
 import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.ResetAngerGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.TemptGoal;
 import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
@@ -39,17 +49,27 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.DrinkHelper;
 import net.minecraft.util.Hand;
+import net.minecraft.util.RangedInteger;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.TickRangeConverter;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.IForgeShearable;
 
 @SuppressWarnings("deprecation")
-public class YakEntity extends AnimalEntity implements IForgeShearable, IShearable {
+public class YakEntity extends AnimalEntity implements IForgeShearable, IShearable, IAngerable {
 	private static final DataParameter<Boolean> SHEARED = EntityDataManager.createKey(YakEntity.class, DataSerializers.BOOLEAN);
+	private static final DataParameter<Integer> ANGER_TIME = EntityDataManager.createKey(YakEntity.class, DataSerializers.VARINT);
+	private static final RangedInteger ANGER_RANGE = TickRangeConverter.convertRange(20, 39);
+
+	private static final UUID SPEED_UUID = UUID.fromString("49455A49-7EC5-45BA-B886-3B90B23A1718");
+	private static final AttributeModifier ATTACKING_SPEED_BOOST = new AttributeModifier(SPEED_UUID, "Attacking speed boost", 0.05D, AttributeModifier.Operation.ADDITION);
+
+	private UUID lastHurtBy;
 	private int timer;
 	private EatGrassGoal eatGrassGoal;
 
@@ -60,6 +80,7 @@ public class YakEntity extends AnimalEntity implements IForgeShearable, IShearab
 	@Override
 	protected void registerGoals() {
 		this.eatGrassGoal = new EatGrassGoal(this);
+		this.goalSelector.addGoal(0, new SwimGoal(this));
 		this.targetSelector.addGoal(1, new HurtByTargetGoal(this).setCallsForHelp(YakEntity.class));
 //		this.goalSelector.addGoal(1, new YakChargeGoal(this));
 		this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
@@ -69,29 +90,50 @@ public class YakEntity extends AnimalEntity implements IForgeShearable, IShearab
 		this.goalSelector.addGoal(5, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
 		this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 6.0F));
 		this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
+
+		this.targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::func_233680_b_));
+		this.targetSelector.addGoal(1, new AngerGoal(this).setCallsForHelp(YakEntity.class));
+		this.targetSelector.addGoal(2, new ResetAngerGoal<>(this, true));
 	}
 
 	@Override
 	protected void registerData() {
 		super.registerData();
 		this.dataManager.register(SHEARED, false);
+		this.dataManager.register(ANGER_TIME, 0);
 	}
 
 	@Override
 	public void writeAdditional(CompoundNBT compound) {
 		super.writeAdditional(compound);
 		compound.putBoolean("Sheared", this.getSheared());
+		this.writeAngerNBT(compound);
 	}
 
 	@Override
 	public void readAdditional(CompoundNBT compound) {
 		super.readAdditional(compound);
 		this.setSheared(compound.getBoolean("Sheared"));
+		this.readAngerNBT((ServerWorld) this.world, compound);
 	}
 
 	@Override
 	protected void updateAITasks() {
 		this.timer = this.eatGrassGoal.getEatingGrassTimer();
+		ModifiableAttributeInstance modifiableattributeinstance = this.getAttribute(Attributes.MOVEMENT_SPEED);
+		if (this.func_233678_J__()) {
+			if (!this.isChild() && !modifiableattributeinstance.hasModifier(ATTACKING_SPEED_BOOST)) {
+				modifiableattributeinstance.applyNonPersistentModifier(ATTACKING_SPEED_BOOST);
+			}
+		} else if (modifiableattributeinstance.hasModifier(ATTACKING_SPEED_BOOST)) {
+			modifiableattributeinstance.removeModifier(ATTACKING_SPEED_BOOST);
+		}
+
+		this.func_241359_a_((ServerWorld) this.world, true);
+
+		if (this.func_233678_J__()) {
+			this.recentlyHit = this.ticksExisted;
+		}
 		super.updateAITasks();
 	}
 
@@ -163,7 +205,7 @@ public class YakEntity extends AnimalEntity implements IForgeShearable, IShearab
 	}
 
 	public static AttributeModifierMap.MutableAttribute registerAttributes() {
-		return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 25.0D).createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2F).createMutableAttribute(Attributes.ATTACK_DAMAGE, 3.0F);
+		return AnimalEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 25.0D).createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2F).createMutableAttribute(Attributes.ATTACK_DAMAGE, 3.0F);
 	}
 
 	@Override
@@ -202,6 +244,8 @@ public class YakEntity extends AnimalEntity implements IForgeShearable, IShearab
 		world.playMovingSound(null, this, SoundEvents.ENTITY_SHEEP_SHEAR, player == null ? SoundCategory.BLOCKS : SoundCategory.PLAYERS, 1.0F, 1.0F);
 		if (!world.isRemote) {
 			this.setSheared(true);
+			if (!(player.getItemStackFromSlot(EquipmentSlotType.LEGS).getItem() instanceof YakPantsItem))
+				this.setAttackTarget(player);
 			int i = 4 + this.rand.nextInt(12);
 
 			java.util.List<ItemStack> items = new java.util.ArrayList<>();
@@ -241,5 +285,50 @@ public class YakEntity extends AnimalEntity implements IForgeShearable, IShearab
 			}
 		}
 
+	}
+
+	@Override
+	public int getAngerTime() {
+		return this.dataManager.get(ANGER_TIME);
+	}
+
+	@Override
+	public void setAngerTime(int time) {
+		this.dataManager.set(ANGER_TIME, time);
+	}
+
+	@Override
+	public UUID getAngerTarget() {
+		return this.lastHurtBy;
+	}
+
+	@Override
+	public void setAngerTarget(UUID target) {
+		this.lastHurtBy = target;
+	}
+
+	@Override
+	public void func_230258_H__() {
+		this.setAngerTime(ANGER_RANGE.func_233018_a_(this.rand));
+	}
+
+	public boolean func_230292_f_(PlayerEntity p_230292_1_) {
+		return this.func_233680_b_(p_230292_1_);
+	}
+
+	class AngerGoal extends HurtByTargetGoal {
+		AngerGoal(YakEntity entity) {
+			super(entity);
+		}
+
+		public boolean shouldContinueExecuting() {
+			return YakEntity.this.func_233678_J__() && super.shouldContinueExecuting();
+		}
+
+		protected void setAttackTarget(MobEntity mobIn, LivingEntity targetIn) {
+			if (mobIn instanceof YakEntity && this.goalOwner.canEntityBeSeen(targetIn)) {
+				mobIn.setAttackTarget(targetIn);
+			}
+		}
 	}
 }
