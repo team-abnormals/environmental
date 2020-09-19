ed335c43018db50eab61f3cc0a5e1bafe2ed2b8b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-09-19 15:05:01 -0500

    Updated Wisteria Leaves code

--- src/main/java/com/minecraftabnormals/environmental/common/block/WisteriaLeavesBlock.java
+++ src/main/java/com/minecraftabnormals/environmental/common/block/WisteriaLeavesBlock.java
@@ -1,49 +1,37 @@
 package com.minecraftabnormals.environmental.common.block;
 
-import com.teamabnormals.abnormals_core.core.utils.ItemStackUtils;
+import java.util.Random;
+
+import com.teamabnormals.abnormals_core.common.blocks.wood.AbnormalsLeavesBlock;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
-import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.IForgeShearable;
 
-import java.util.Random;
-
-public class WisteriaLeavesBlock extends Block implements IForgeShearable {
-    public static final IntegerProperty DISTANCE = IntegerProperty.create("distance", 1, 8);
-    public static final BooleanProperty PERSISTENT = BlockStateProperties.PERSISTENT;
+public class WisteriaLeavesBlock extends AbnormalsLeavesBlock implements IForgeShearable {
+    public static final IntegerProperty DISTANCE_CLONE = IntegerProperty.create("distance_clone", 1, 8);
 
     public WisteriaLeavesBlock(Block.Properties properties) {
         super(properties);
-        setDefaultState(stateContainer.getBaseState().with(DISTANCE, 8).with(PERSISTENT, false));
+        this.setDefaultState(stateContainer.getBaseState().with(DISTANCE_CLONE, 8).with(PERSISTENT, false));
     }
 
     @Override
     public boolean ticksRandomly(BlockState state) {
-        return state.get(DISTANCE) == 8 && !state.get(PERSISTENT);
+        return state.get(DISTANCE_CLONE) == 8 && !state.get(PERSISTENT);
     }
 
     @Override
     public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
-        if (!state.get(PERSISTENT) && state.get(DISTANCE) == 8) {
+        if (!state.get(PERSISTENT) && state.get(DISTANCE_CLONE) == 8) {
             spawnDrops(state, worldIn, pos);
             worldIn.removeBlock(pos, false);
         }
@@ -55,21 +43,16 @@ public class WisteriaLeavesBlock extends Block implements IForgeShearable {
     }
 
     @Override
-    public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
-        return 1;
-    }
-
-    @Override
     public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
         int i = getDistance(facingState) + 1;
-        if (i != 1 || stateIn.get(DISTANCE) != i) {
+        if (i != 1 || stateIn.get(DISTANCE_CLONE) != i) {
             worldIn.getPendingBlockTicks().scheduleTick(currentPos, this, 1);
         }
         return stateIn;
     }
 
     private static BlockState updateDistance(BlockState state, IWorld worldIn, BlockPos pos) {
-        int i = 7;
+        int i = 8;
         BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
 
         for (Direction direction : Direction.values()) {
@@ -80,55 +63,24 @@ public class WisteriaLeavesBlock extends Block implements IForgeShearable {
             }
         }
 
-        return state.with(DISTANCE, Integer.valueOf(i));
+        return state.with(DISTANCE_CLONE, Integer.valueOf(i));
     }
 
     private static int getDistance(BlockState neighbor) {
         if (BlockTags.LOGS.contains(neighbor.getBlock())) {
             return 0;
         } else {
-            if (neighbor.getBlock() instanceof WisteriaLeavesBlock) return neighbor.get(DISTANCE);
-            if (neighbor.getBlock() instanceof LeavesBlock) return neighbor.get(LeavesBlock.DISTANCE);
+            if (neighbor.getBlock() instanceof WisteriaLeavesBlock) 
+            	return neighbor.get(DISTANCE_CLONE);
+            else if (neighbor.getBlock() instanceof LeavesBlock) 
+            	return neighbor.get(LeavesBlock.DISTANCE);
             else return 8;
         }
     }
 
     @Override
-    @OnlyIn(Dist.CLIENT)
-    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-        if (worldIn.isRainingAt(pos.up())) {
-            if (rand.nextInt(15) == 1) {
-                BlockPos blockpos = pos.down();
-                BlockState blockstate = worldIn.getBlockState(blockpos);
-                if (!blockstate.isSolid() || !blockstate.isSolidSide(worldIn, blockpos, Direction.UP)) {
-                    double d0 = (double) ((float) pos.getX() + rand.nextFloat());
-                    double d1 = (double) pos.getY() - 0.05D;
-                    double d2 = (double) ((float) pos.getZ() + rand.nextFloat());
-                    worldIn.addParticle(ParticleTypes.DRIPPING_WATER, d0, d1, d2, 0.0D, 0.0D, 0.0D);
-                }
-            }
-        }
-    }
-
-    @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(DISTANCE, PERSISTENT);
-    }
-
-    @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return updateDistance(getDefaultState().with(PERSISTENT, true), context.getWorld(), context.getPos());
-    }
-
-    @Override
-    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
-        if (ItemStackUtils.isInGroup(this.asItem(), group)) {
-            int targetIndex = ItemStackUtils.findIndexOfItem(Items.DARK_OAK_LEAVES, items);
-            if (targetIndex != -1) {
-                items.add(targetIndex + 1, new ItemStack(this));
-            } else {
-                super.fillItemGroup(group, items);
-            }
-        }
+    	super.fillStateContainer(builder);
+        builder.add(DISTANCE_CLONE);
     }
 }
