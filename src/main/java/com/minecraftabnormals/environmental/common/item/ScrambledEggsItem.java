06e851cb17d0025e94c08c15bff05106f5c857a1
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-02-04 14:19:26 -0600

    Made ScrambledEggs properly extend SoupItem

--- src/main/java/com/minecraftabnormals/environmental/common/item/ScrambledEggsItem.java
+++ src/main/java/com/minecraftabnormals/environmental/common/item/ScrambledEggsItem.java
@@ -1,20 +1,18 @@
 package com.minecraftabnormals.environmental.common.item;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.SoupItem;
 import net.minecraft.world.World;
 
-public class ScrambledEggsItem extends Item {
+public class ScrambledEggsItem extends SoupItem {
 	public ScrambledEggsItem(Item.Properties properties) {
 		super(properties);
 	}
 
 	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
-		ItemStack itemstack = super.onItemUseFinish(stack, worldIn, entityLiving);
 		entityLiving.heal(2.0F);
-		return entityLiving instanceof PlayerEntity && ((PlayerEntity) entityLiving).abilities.isCreativeMode ? itemstack : new ItemStack(Items.BOWL);
+		return super.onItemUseFinish(stack, worldIn, entityLiving);
 	}
 }
