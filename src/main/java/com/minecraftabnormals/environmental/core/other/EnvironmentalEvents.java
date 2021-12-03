e895d545443c920d83eff99bed222f8ca855f735
Bob Dole <8822548+wchen1990@users.noreply.github.com>
2021-12-02 23:14:35 -0500

    Add additional checks for Pig TemptGoal (#237)
    - Fix issues where sometimes some other mod author diddles with vanilla
      mob entities and adds additional Navigators. This commit makes it so
      that it'll only ever add these goals to Pig entities with vanilla
      navigators.

--- src/main/java/com/minecraftabnormals/environmental/core/other/EnvironmentalEvents.java
+++ src/main/java/com/minecraftabnormals/environmental/core/other/EnvironmentalEvents.java
@@ -44,6 +44,8 @@ import net.minecraft.item.ShovelItem;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.pathfinding.FlyingPathNavigator;
+import net.minecraft.pathfinding.GroundPathNavigator;
 import net.minecraft.potion.*;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
@@ -383,7 +385,7 @@ public class EnvironmentalEvents {
 			Set<PrioritizedGoal> goals = pig.goalSelector.availableGoals;
 			if (goals.stream().noneMatch((goal) -> goal.getGoal() instanceof HuntTruffleGoal))
 				pig.goalSelector.addGoal(2, new HuntTruffleGoal(pig));
-			if (goals.stream().noneMatch((goal) -> goal.getGoal() instanceof TemptGoldenCarrotGoal))
+			if ((pig.getNavigation() instanceof GroundPathNavigator || pig.getNavigation() instanceof FlyingPathNavigator) && goals.stream().noneMatch((goal) -> goal.getGoal() instanceof TemptGoldenCarrotGoal))
 				pig.goalSelector.addGoal(4, new TemptGoldenCarrotGoal(pig, 1.2D, false, Ingredient.of(Items.GOLDEN_CARROT)));
 		}
 	}
