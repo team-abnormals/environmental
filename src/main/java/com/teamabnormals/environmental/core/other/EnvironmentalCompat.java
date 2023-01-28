afe48e99af3e108e64c9e2c6380298f0fd51e576
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-01-28 16:27:09 -0600

    Some cherry tree shape modifications

--- src/main/java/com/teamabnormals/environmental/core/other/EnvironmentalCompat.java
+++ src/main/java/com/teamabnormals/environmental/core/other/EnvironmentalCompat.java
@@ -33,6 +33,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class EnvironmentalCompat {
+	
 	public static void registerCompat() {
 		registerLootInjectors();
 		registerCompostables();
