1a740852438d9fa42a97ae18366491715ee04a08
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-01-30 15:05:43 -0600

    Fix some outdated code

--- src/main/java/com/teamabnormals/environmental/common/block/LargeLilyPadBlock.java
+++ src/main/java/com/teamabnormals/environmental/common/block/LargeLilyPadBlock.java
@@ -1,9 +1,11 @@
 package com.teamabnormals.environmental.common.block;
 
 import com.teamabnormals.blueprint.core.util.item.filling.TargetedItemCategoryFiller;
+import com.teamabnormals.environmental.core.registry.EnvironmentalSoundEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -35,32 +37,13 @@ import net.minecraftforge.common.PlantType;
 import javax.annotation.Nullable;
 
 public class LargeLilyPadBlock extends BushBlock implements IPlantable {
-	protected static final VoxelShape GIANT_LILY_PAD_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1.5D, 16.0D);
+	protected static final VoxelShape GIANT_LILY_PAD_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.2001D, 16.0D);
 	public static final EnumProperty<LilyPadPosition> POSITION = EnumProperty.create("position", LilyPadPosition.class);
 	private static final TargetedItemCategoryFiller FILLER = new TargetedItemCategoryFiller(() -> Items.LILY_PAD);
 
 	public LargeLilyPadBlock(BlockBehaviour.Properties builder) {
 		super(builder);
 	}
-
-//	@SuppressWarnings("deprecation")
-//	@Override
-//	public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
-//		super.onEntityCollision(state, worldIn, pos, entityIn);
-//		if (entityIn.isSuppressingBounce()) {
-//			super.onLanded(worldIn, entityIn);
-//		} else {
-//			this.bounce(entityIn);
-//		}	
-//	}
-//
-//	public void bounce(Entity entity) {
-//		Vector3d vector3d = entity.getMotion();
-//		if (vector3d.y < 0.0D) {
-//			double d0 = entity instanceof LivingEntity ? 1.5D : 1.2D;
-//			entity.setMotion(vector3d.x, -vector3d.y * d0, vector3d.z);
-//		}
-//	}
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
@@ -93,23 +76,26 @@ public class LargeLilyPadBlock extends BushBlock implements IPlantable {
 
 	@Override
 	public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
-		super.fallOn(level, state, pos, entity, fallDistance * 0.25F);
+		super.fallOn(level, state, pos, entity, 0.0F);
 	}
 
 	@Override
-	public void updateEntityAfterFallOn(BlockGetter worldIn, Entity entityIn) {
-		if (entityIn.isSuppressingBounce()) {
-			super.updateEntityAfterFallOn(worldIn, entityIn);
+	public void updateEntityAfterFallOn(BlockGetter level, Entity entity) {
+		if (entity.isSuppressingBounce()) {
+			super.updateEntityAfterFallOn(level, entity);
 		} else {
-			this.bounce(entityIn);
+			this.bounce(entity);
 		}
 	}
 
 	private void bounce(Entity entity) {
 		Vec3 vector3d = entity.getDeltaMovement();
 		if (vector3d.y < 0.0D) {
-			double d0 = entity instanceof LivingEntity ? 1.5D : 1.2D;
+			double d0 = entity instanceof LivingEntity ? 1.3D : 0.9D;
 			entity.setDeltaMovement(vector3d.x, -vector3d.y * d0, vector3d.z);
+			if (vector3d.y < -0.1D) {
+				entity.getLevel().playSound(entity instanceof Player player ? player : null, entity.getX(), entity.getY(), entity.getZ(), EnvironmentalSoundEvents.LARGE_LILY_PAD_BOUNCE.get(), SoundSource.BLOCKS, 1.0F, 1.0F);
+			}
 		}
 	}
 
@@ -180,29 +166,19 @@ public class LargeLilyPadBlock extends BushBlock implements IPlantable {
 
 	public static BlockPos posToBlockPos(LilyPadPosition position, BlockPos pos, boolean revert) {
 		if (!revert) {
-			switch (position) {
-				case NORTHEAST:
-					return pos.relative(Direction.NORTH).relative(Direction.EAST);
-				case NORTHWEST:
-					return pos.relative(Direction.NORTH);
-				case SOUTHEAST:
-					return pos.relative(Direction.EAST);
-				default:
-				case SOUTHWEST:
-					return pos;
-			}
+			return switch (position) {
+				case NORTHEAST -> pos.relative(Direction.NORTH).relative(Direction.EAST);
+				case NORTHWEST -> pos.relative(Direction.NORTH);
+				case SOUTHEAST -> pos.relative(Direction.EAST);
+				case SOUTHWEST -> pos;
+			};
 		} else {
-			switch (position) {
-				case NORTHEAST:
-					return pos.relative(Direction.SOUTH).relative(Direction.WEST);
-				case NORTHWEST:
-					return pos.relative(Direction.SOUTH);
-				case SOUTHEAST:
-					return pos.relative(Direction.WEST);
-				default:
-				case SOUTHWEST:
-					return pos;
-			}
+			return switch (position) {
+				case NORTHEAST -> pos.relative(Direction.SOUTH).relative(Direction.WEST);
+				case NORTHWEST -> pos.relative(Direction.SOUTH);
+				case SOUTHEAST -> pos.relative(Direction.WEST);
+				case SOUTHWEST -> pos;
+			};
 		}
 	}
 
