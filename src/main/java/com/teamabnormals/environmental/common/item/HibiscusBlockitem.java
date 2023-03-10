1897622f0a53cc03f94dba21ed0c4965184c5c16
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2023-03-10 17:16:44 +0200

    Hibiscus changes

---
+++ src/main/java/com/teamabnormals/environmental/common/item/HibiscusBlockitem.java
@@ -0,0 +1,39 @@
+package com.teamabnormals.environmental.common.item;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.StandingAndWallBlockItem;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+
+import javax.annotation.Nullable;
+
+public class HibiscusBlockitem extends StandingAndWallBlockItem {
+
+	public HibiscusBlockitem(Block block, Block wallBlock, Properties properties) {
+		super(block, wallBlock, properties);
+	}
+
+	@Nullable
+	protected BlockState getPlacementState(BlockPlaceContext context) {
+		BlockState blockstate = this.wallBlock.getStateForPlacement(context);
+		BlockState blockstate1 = null;
+		LevelReader level = context.getLevel();
+		BlockPos blockpos = context.getClickedPos();
+
+		for(Direction direction : context.getNearestLookingDirections()) {
+			boolean flag = direction == Direction.DOWN && !(level.getBlockState(blockpos.below()).getBlock() instanceof LeavesBlock);
+			BlockState blockstate2 = flag ? this.getBlock().getStateForPlacement(context) : blockstate;
+			if (blockstate2 != null && blockstate2.canSurvive(level, blockpos)) {
+				blockstate1 = blockstate2;
+				break;
+			}
+		}
+
+		return blockstate1 != null && level.isUnobstructed(blockstate1, blockpos, CollisionContext.empty()) ? blockstate1 : null;
+	}
+}
