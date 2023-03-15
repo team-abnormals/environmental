4a3e2ddafa3d1165a0f6fef0770ff7d39e30bf08
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-15 16:06:45 -0500

    Change a material

--- src/main/java/com/teamabnormals/environmental/core/other/EnvironmentalTiers.java
+++ src/main/java/com/teamabnormals/environmental/core/other/EnvironmentalTiers.java
@@ -10,7 +10,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 
 public class EnvironmentalTiers {
 	public static class Armor {
-		public static final BlueprintArmorMaterial EXPLORER = new BlueprintArmorMaterial(new ResourceLocation(Environmental.MOD_ID, "explorer"), 12, new int[]{2, 3, 2, 3}, 17, () -> SoundEvents.ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> Ingredient.of(Items.RABBIT_HIDE));
+		public static final BlueprintArmorMaterial EXPLORER = new BlueprintArmorMaterial(new ResourceLocation(Environmental.MOD_ID, "explorer"), 12, new int[]{2, 3, 2, 3}, 17, () -> SoundEvents.ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> Ingredient.of(Items.LEATHER));
 		public static final BlueprintArmorMaterial YAK = new BlueprintArmorMaterial(new ResourceLocation(Environmental.MOD_ID, "yak"), 5, new int[]{3, 3, 3, 3}, 15, () -> SoundEvents.ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> Ingredient.of(EnvironmentalItems.YAK_HAIR.get()));
 	}
 }
