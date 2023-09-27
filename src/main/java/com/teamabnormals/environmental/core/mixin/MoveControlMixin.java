e8680b05a28dbded0af589235daca604e8426d3c
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-09-27 03:03:49 -0500

    Some Tapir AI development

---
+++ src/main/java/com/teamabnormals/environmental/core/mixin/MoveControlMixin.java
@@ -0,0 +1,27 @@
+package com.teamabnormals.environmental.core.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.control.MoveControl;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MoveControl.class)
+public abstract class MoveControlMixin {
+
+	@Shadow
+	@Final
+	protected Mob mob;
+
+	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
+	private VoxelShape place(BlockState state, BlockGetter level, BlockPos pos) {
+		return state.getCollisionShape(level, pos, CollisionContext.of(this.mob));
+	}
+}
