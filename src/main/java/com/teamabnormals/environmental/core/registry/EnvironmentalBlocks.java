40bc61fbe2332c9bc9185071f41b36e48403d430
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-10-24 14:24:15 -0500

    Fix Slabfish Effigy break/place sound
    Closes https://github.com/team-abnormals/environmental/issues/365

--- src/main/java/com/teamabnormals/environmental/core/registry/EnvironmentalBlocks.java
+++ src/main/java/com/teamabnormals/environmental/core/registry/EnvironmentalBlocks.java
@@ -53,7 +53,7 @@ public class EnvironmentalBlocks {
 	public static final RegistryObject<Block> SMOOTH_MUD_SLAB = HELPER.createBlock("smooth_mud_slab", () -> new SlabBlock(EnvironmentalProperties.MUD_BRICKS), CreativeModeTab.TAB_BUILDING_BLOCKS);
 	public static final RegistryObject<Block> SMOOTH_MUD_VERTICAL_SLAB = HELPER.createCompatBlock("quark", "smooth_mud_vertical_slab", () -> new DirectionalVerticalSlabBlock(EnvironmentalProperties.MUD_BRICKS), CreativeModeTab.TAB_BUILDING_BLOCKS);
 	public static final RegistryObject<Block> CHISELED_MUD_BRICKS = HELPER.createBlock("chiseled_mud_bricks", () -> new Block(EnvironmentalProperties.MUD_BRICKS), CreativeModeTab.TAB_BUILDING_BLOCKS);
-	public static final RegistryObject<Block> SLABFISH_EFFIGY = HELPER.createBlock("slabfish_effigy", () -> new SlabfishEffigyBlock(PropertyUtil.flowerPot()), CreativeModeTab.TAB_DECORATIONS);
+	public static final RegistryObject<Block> SLABFISH_EFFIGY = HELPER.createBlock("slabfish_effigy", () -> new SlabfishEffigyBlock(PropertyUtil.flowerPot().sound(SoundType.MUD_BRICKS)), CreativeModeTab.TAB_DECORATIONS);
 
 	// Crops //
 
