186ea0973bc61451cdc3d5a6befe6ecc255ebd75
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-11-15 20:29:04 -0600

    Pinecone particles

--- src/main/java/com/teamabnormals/environmental/common/block/PineconeBlock.java
+++ src/main/java/com/teamabnormals/environmental/common/block/PineconeBlock.java
@@ -4,10 +4,12 @@ import com.google.common.collect.Lists;
 import com.teamabnormals.environmental.core.registry.EnvironmentalBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.item.BoneMealItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FallingBlock;
@@ -18,88 +20,120 @@ import java.util.List;
 
 public class PineconeBlock extends FallingBlock {
 
-    public PineconeBlock(Properties properties) {
-        super(properties);
-    }
+	public PineconeBlock(Properties properties) {
+		super(properties);
+	}
 
-    @Override
-    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        if (canFall(level, pos) && pos.getY() >= level.getMinBuildHeight()) {
-            FallingBlockEntity fallingblockentity = FallingBlockEntity.fall(level, pos, state);
-            this.falling(fallingblockentity);
-        }
-    }
+	@Override
+	public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+		if (canFall(level, pos) && pos.getY() >= level.getMinBuildHeight()) {
+			FallingBlockEntity fallingblockentity = FallingBlockEntity.fall(level, pos, state);
+			this.falling(fallingblockentity);
+		}
+	}
 
-    @Override
-    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        if (random.nextInt(3) == 0)
-            return;
+	@Override
+	public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+		if (random.nextInt(3) != 0) {
 
-        List<BlockPos> list = Lists.newArrayList();
-        addPossibleSaplingPositionsFromNeighbors(4, 4, 4, list, level, pos.offset(-4, -4, -4), new BlockPos.MutableBlockPos(), new boolean[9][9][9]);
+			BlockState sapling = EnvironmentalBlocks.PINE_SAPLING.get().defaultBlockState();
+			List<BlockPos> list = Lists.newArrayList();
+			addPossibleSaplingPositionsFromNeighbors(4, 4, 4, list, level, pos.offset(-4, -4, -4), new BlockPos.MutableBlockPos(), new boolean[9][9][9]);
 
-        for (int i = 0; i < 12; i++) {
-            if (list.isEmpty())
-                break;
+			if (!list.isEmpty()) {
+				this.spawnBoneMealParticles(level, this.defaultBlockState(), pos.below(), 4);
+			}
+			for (int i = 0; i < 12; i++) {
+				if (list.isEmpty())
+					break;
 
-            BlockPos blockpos = list.remove(random.nextInt(list.size()));
-            if (level.isAreaLoaded(blockpos, 1) && level.getMaxLocalRawBrightness(blockpos.above()) >= 9 && hasSpaceForTree(blockpos, level)) {
-                level.setBlock(blockpos, EnvironmentalBlocks.PINE_SAPLING.get().defaultBlockState(), 2);
-                break;
-            }
-        }
-    }
+				BlockPos blockpos = list.remove(random.nextInt(list.size()));
+				if (level.isAreaLoaded(blockpos, 1) && level.getMaxLocalRawBrightness(blockpos.above()) >= 9 && hasSpaceForTree(blockpos, level)) {
+					level.setBlockAndUpdate(blockpos, sapling);
+					this.spawnBoneMealParticles(level, sapling, blockpos, 15);
+					return;
+				}
+			}
+		}
+	}
 
-    private static void addPossibleSaplingPositions(int x, int y, int z, List<BlockPos> list, ServerLevel level, BlockPos origin, BlockPos.MutableBlockPos mutablepos, boolean[][][] foundpositions) {
-        if (x < 0 || x >= 9 || y < 0 || y >= 9 || z < 0 || z >= 9)
-            return;
-        else if (foundpositions[x][y][z])
-            return;
+	private void spawnBoneMealParticles(ServerLevel level, BlockState state, BlockPos pos, int amount) {
+		double d0 = 0.5D;
+		double d1;
+		if (state.isSolidRender(level, pos)) {
+			pos = pos.above();
+			amount *= 3;
+			d0 = 3.0D;
+			d1 = 1.0D;
+		} else {
+			d1 = state.getShape(level, pos).max(Direction.Axis.Y);
+		}
 
-        mutablepos.setWithOffset(origin, x, y, z);
-        foundpositions[x][y][z] = true;
+		level.sendParticles(ParticleTypes.HAPPY_VILLAGER, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, 0, 0.0D, 0.0D, 0.0D, 0.0D);
+		RandomSource random = level.getRandom();
 
-        if (level.getBlockState(mutablepos).is(BlockTags.DIRT)) {
-            if (level.isEmptyBlock(mutablepos.move(Direction.UP)))
-                list.add(mutablepos.immutable());
+		for (int i = 0; i < amount; ++i) {
+			double d2 = random.nextGaussian() * 0.02D;
+			double d3 = random.nextGaussian() * 0.02D;
+			double d4 = random.nextGaussian() * 0.02D;
+			double d5 = 0.5D - d0;
+			double d6 = (double) pos.getX() + d5 + random.nextDouble() * d0 * 2.0D;
+			double d7 = (double) pos.getY() + random.nextDouble() * d1;
+			double d8 = (double) pos.getZ() + d5 + random.nextDouble() * d0 * 2.0D;
+			level.sendParticles(ParticleTypes.HAPPY_VILLAGER, d6, d7, d8, 0, d2, d3, d4, 0.0D);
+		}
+	}
 
-            addPossibleSaplingPositionsFromNeighbors(x, y, z, list, level, origin, mutablepos, foundpositions);
-        }
-    }
+	private static void addPossibleSaplingPositions(int x, int y, int z, List<BlockPos> list, ServerLevel level, BlockPos origin, BlockPos.MutableBlockPos mutablepos, boolean[][][] foundpositions) {
+		if (x < 0 || x >= 9 || y < 0 || y >= 9 || z < 0 || z >= 9)
+			return;
+		else if (foundpositions[x][y][z])
+			return;
 
-    private static void addPossibleSaplingPositionsFromNeighbors(int x, int y, int z, List<BlockPos> list, ServerLevel level, BlockPos origin, BlockPos.MutableBlockPos mutablepos, boolean[][][] foundpositions) {
-        addPossibleSaplingPositions(x - 1, y, z, list, level, origin, mutablepos, foundpositions);
-        addPossibleSaplingPositions(x + 1, y, z, list, level, origin, mutablepos, foundpositions);
-        addPossibleSaplingPositions(x, y - 1, z, list, level, origin, mutablepos, foundpositions);
-        addPossibleSaplingPositions(x, y + 1, z, list, level, origin, mutablepos, foundpositions);
-        addPossibleSaplingPositions(x, y, z - 1, list, level, origin, mutablepos, foundpositions);
-        addPossibleSaplingPositions(x, y, z + 1, list, level, origin, mutablepos, foundpositions);
-    }
+		mutablepos.setWithOffset(origin, x, y, z);
+		foundpositions[x][y][z] = true;
 
-    private static boolean hasSpaceForTree(BlockPos pos, ServerLevel level) {
-        BlockPos.MutableBlockPos mutablepos = new BlockPos.MutableBlockPos();
-        for (int y = -2; y <= 3; y++) {
-            for (int x = -3; x <= 3; x++) {
-                for (int z = -3; z <= 3; z++) {
-                    mutablepos.setWithOffset(pos, x, y, z);
-                    if ((y > 0 && !TreeFeature.validTreePos(level, mutablepos)) || level.getBlockState(mutablepos).is(BlockTags.SAPLINGS))
-                        return false;
-                }
-            }
-        }
-        return true;
-    }
+		if (level.getBlockState(mutablepos).is(BlockTags.DIRT)) {
+			if (level.isEmptyBlock(mutablepos.move(Direction.UP)))
+				list.add(mutablepos.immutable());
 
-    public static boolean canFall(ServerLevel level, BlockPos pos) {
-        return !supportedFromAbove(level, pos) && isFree(level.getBlockState(pos.below()));
-    }
+			addPossibleSaplingPositionsFromNeighbors(x, y, z, list, level, origin, mutablepos, foundpositions);
+		}
+	}
 
-    public static boolean supportedFromAbove(ServerLevel level, BlockPos pos) {
-        BlockState blockstate = level.getBlockState(pos.above());
-        return Block.isFaceFull(blockstate.getCollisionShape(level, pos.above()), Direction.DOWN) && !(blockstate.getBlock() instanceof FallingBlock);
-    }
+	private static void addPossibleSaplingPositionsFromNeighbors(int x, int y, int z, List<BlockPos> list, ServerLevel level, BlockPos origin, BlockPos.MutableBlockPos mutablepos, boolean[][][] foundpositions) {
+		addPossibleSaplingPositions(x - 1, y, z, list, level, origin, mutablepos, foundpositions);
+		addPossibleSaplingPositions(x + 1, y, z, list, level, origin, mutablepos, foundpositions);
+		addPossibleSaplingPositions(x, y - 1, z, list, level, origin, mutablepos, foundpositions);
+		addPossibleSaplingPositions(x, y + 1, z, list, level, origin, mutablepos, foundpositions);
+		addPossibleSaplingPositions(x, y, z - 1, list, level, origin, mutablepos, foundpositions);
+		addPossibleSaplingPositions(x, y, z + 1, list, level, origin, mutablepos, foundpositions);
+	}
 
-    @Override
-    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-    }
+	private static boolean hasSpaceForTree(BlockPos pos, ServerLevel level) {
+		BlockPos.MutableBlockPos mutablepos = new BlockPos.MutableBlockPos();
+		for (int y = -2; y <= 3; y++) {
+			for (int x = -3; x <= 3; x++) {
+				for (int z = -3; z <= 3; z++) {
+					mutablepos.setWithOffset(pos, x, y, z);
+					if ((y > 0 && !TreeFeature.validTreePos(level, mutablepos)) || level.getBlockState(mutablepos).is(BlockTags.SAPLINGS))
+						return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static boolean canFall(ServerLevel level, BlockPos pos) {
+		return !supportedFromAbove(level, pos) && isFree(level.getBlockState(pos.below()));
+	}
+
+	public static boolean supportedFromAbove(ServerLevel level, BlockPos pos) {
+		BlockState blockstate = level.getBlockState(pos.above());
+		return Block.isFaceFull(blockstate.getCollisionShape(level, pos.above()), Direction.DOWN) && !(blockstate.getBlock() instanceof FallingBlock);
+	}
+
+	@Override
+	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+	}
 }
