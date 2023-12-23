479a84979fd54583212e56fd5239b068295ebc70
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2023-12-23 15:34:57 +0200

    Fix

--- src/main/java/com/teamabnormals/environmental/common/damagesource/MountDamageSource.java
+++ src/main/java/com/teamabnormals/environmental/common/damagesource/MountDamageSource.java
@@ -1,16 +1,33 @@
 package com.teamabnormals.environmental.common.damagesource;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.damagesource.IndirectEntityDamageSource;
+import net.minecraft.world.damagesource.EntityDamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 
-public class MountDamageSource extends IndirectEntityDamageSource {
+import javax.annotation.Nullable;
 
-	public MountDamageSource(String msgId, Entity passenger, Entity mount) {
-		super(msgId, passenger, mount);
+public class MountDamageSource extends EntityDamageSource {
+	private final Entity rider;
+
+	public MountDamageSource(String msgId, Entity mount, Entity rider) {
+		super(msgId, mount);
+		this.rider = rider;
 	}
 
+	@Nullable
+	@Override
+	public Entity getDirectEntity() {
+		return this.entity;
+	}
+
+	@Nullable
+	@Override
+	public Entity getEntity() {
+		return this.rider;
+	}
+
+	@Override
 	public Component getLocalizedDeathMessage(LivingEntity p_19410_) {
 		return Component.translatable("death.attack." + this.msgId, p_19410_.getDisplayName(), this.getEntity().getDisplayName(), this.getDirectEntity().getDisplayName());
 	}
