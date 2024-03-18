05e27b34312b1392b0f3a3bc53913c18208c6b17
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-03-18 09:11:20 -0500

    Duck fix
    Closes #399

--- src/main/java/com/teamabnormals/environmental/common/entity/animal/Duck.java
+++ src/main/java/com/teamabnormals/environmental/common/entity/animal/Duck.java
@@ -53,14 +53,15 @@ public class Duck extends Animal {
 
 	@Override
 	protected void registerGoals() {
-		this.goalSelector.addGoal(0, new DuckSwimGoal(this));
+		this.goalSelector.addGoal(0, new FloatGoal(this));
 		this.goalSelector.addGoal(1, new PanicGoal(this, 1.4D));
 		this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
 		this.goalSelector.addGoal(3, new TemptGoal(this, 1.0D, Ingredient.of(EnvironmentalItemTags.DUCK_FOOD), false));
 		this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
-		this.goalSelector.addGoal(5, new RandomStrollGoal(this, 1.0D));
-		this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
-		this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(5, new DuckSwimGoal(this));
+		this.goalSelector.addGoal(6, new RandomStrollGoal(this, 1.0D));
+		this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 6.0F));
+		this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
 	}
 
 	@Override
