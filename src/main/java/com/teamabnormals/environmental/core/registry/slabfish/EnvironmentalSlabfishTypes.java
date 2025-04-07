9f53662082fed4c3b76584c519fd5bfa8959f9de
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-04-07 00:28:46 -0500

    Tweak some Slabfish priorities / conditions
    Fixes https://github.com/team-abnormals/neapolitan/issues/219

--- src/main/java/com/teamabnormals/environmental/core/registry/slabfish/EnvironmentalSlabfishTypes.java
+++ src/main/java/com/teamabnormals/environmental/core/registry/slabfish/EnvironmentalSlabfishTypes.java
@@ -91,9 +91,9 @@ public class EnvironmentalSlabfishTypes {
 
 	public static final ResourceKey<SlabfishType> MUSHROOM = createKey("mushroom");
 	public static final ResourceKey<SlabfishType> BROWN_MUSHROOM = createKey("brown_mushroom");
-	public static final ResourceKey<SlabfishType> SKY = createKey("sky");
 	public static final ResourceKey<SlabfishType> DROWNED = createKey("drowned");
 	public static final ResourceKey<SlabfishType> NIGHTMARE = createKey("nightmare");
+	public static final ResourceKey<SlabfishType> SKY = createKey("sky");
 	public static final ResourceKey<SlabfishType> SKELETON = createKey("skeleton");
 	public static final ResourceKey<SlabfishType> STRAY = createKey("stray");
 	public static final ResourceKey<SlabfishType> WITHER = createKey("wither");
@@ -109,62 +109,70 @@ public class EnvironmentalSlabfishTypes {
 		register(context, SWAMP, -1, Tags.Biomes.IS_SWAMP);
 
 		register(context, BADLANDS, 0, BiomeTags.IS_BADLANDS);
-		register(context, BAMBOO, 1, Biomes.BAMBOO_JUNGLE);
-		register(context, BASALT_DELTAS, 1, Biomes.BASALT_DELTAS);
 		register(context, BEACH, 0, spawn(), or(biome(context, BiomeTags.IS_BEACH), biome(context, Biomes.STONY_SHORE)));
-		register(context, BLOSSOM, 1, spawn(), or(biome(context, EnvironmentalBiomes.BLOSSOM_WOODS), biome(context, EnvironmentalBiomes.BLOSSOM_VALLEYS)));
-		register(context, BROWN_MUSHROOM, 2, new SlabfishEventCondition(Event.LIGHTNING), new SlabfishTypeCondition(MUSHROOM.location()));
-		register(context, CAVE, 2, spawn(), new SlabfishHeightCondition(Integer.MIN_VALUE, 48), new SlabfishLightCondition(0, 0, LightLayer.SKY), dimension(BuiltinDimensionTypes.OVERWORLD));
-		register(context, CHERRY_GROVE, 1, Biomes.CHERRY_GROVE);
-		register(context, CHORUS, 1, BlueprintBiomeTags.IS_OUTER_END);
-		register(context, CRIMSON, 1, Biomes.CRIMSON_FOREST);
-		register(context, DARK_FOREST, 1, Biomes.DARK_FOREST);
-		register(context, DEEP_DARK, 5, Biomes.DEEP_DARK);
-		register(context, DEEPSLATE, 3, spawn(), new SlabfishHeightCondition(Integer.MIN_VALUE, 0), new SlabfishLightCondition(0, 0, LightLayer.SKY), dimension(BuiltinDimensionTypes.OVERWORLD));
-		register(context, DRIPSTONE_CAVES, 5, Biomes.DRIPSTONE_CAVES);
 		register(context, DESERT, 0, Biomes.DESERT);
-		register(context, DROWNED, 2, spawn(), biome(context, BiomeTags.IS_OCEAN), new SlabfishHeightCondition(Integer.MIN_VALUE, 48), new SlabfishInFluidCondition(context.lookup(Registries.FLUID).getOrThrow(FluidTags.WATER)));
-		register(context, END, 0, spawn(), dimension(BuiltinDimensionTypes.END));
-		register(context, FLOWER_FOREST, 1, Biomes.FLOWER_FOREST);
 		register(context, FOREST, 0, BiomeTags.IS_FOREST);
-		register(context, FROZEN_OCEAN, 2, spawn(), or(biome(context, Biomes.FROZEN_OCEAN), biome(context, Biomes.DEEP_FROZEN_OCEAN)));
-		register(context, GHOST, 0, new SlabfishImpossibleCondition());
 		register(context, HILL, 0, BiomeTags.IS_HILL);
-		register(context, ICE_SPIKES, 1, Biomes.ICE_SPIKES);
 		register(context, JUNGLE, 0, BiomeTags.IS_JUNGLE);
-		register(context, LUSH_CAVES, 5, Biomes.LUSH_CAVES);
-		register(context, MANGROVE, 1, Biomes.MANGROVE_SWAMP);
-		register(context, MARSH, 1, EnvironmentalBiomes.MARSH);
-		register(context, MOUNTAIN, 0, spawn(), or(biome(context, Biomes.MEADOW), biome(context, Biomes.STONY_PEAKS)));
-		register(context, MUSHROOM, 2, or(and(spawn(), biome(context, Biomes.MUSHROOM_FIELDS)), and(new SlabfishEventCondition(Event.LIGHTNING), new SlabfishTypeCondition(BROWN_MUSHROOM.location()))));
-		register(context, NETHER, 0, spawn(), dimension(BuiltinDimensionTypes.NETHER));
-		register(context, NIGHTMARE, 2, spawn(), new SlabfishInsomniaCondition(), new SlabfishTimeCondition(Time.NIGHT));
 		register(context, OCEAN, 0, BiomeTags.IS_OCEAN);
-		register(context, PINE, 1, spawn(), or(biome(context, EnvironmentalBiomeTags.IS_PINE_BARRENS), biome(context, EnvironmentalBiomes.PINE_SLOPES)));
-		register(context, PLAINS, 0, spawn(), or(biome(context, Biomes.PLAINS), biome(context, Biomes.SUNFLOWER_PLAINS)));
-		register(context, RIVER, 0, BiomeTags.IS_RIVER);
+		register(context, PLAINS, 0, Tags.Biomes.IS_PLAINS);
+		register(context, RIVER, 0, Biomes.RIVER);
 		register(context, SAVANNA, 0, BiomeTags.IS_SAVANNA);
-		register(context, SKELETON, 2, new SlabfishEventCondition(Event.LIGHTNING));
-		register(context, SKY, 2, spawn(), new SlabfishHeightCondition(256, Integer.MAX_VALUE), dimension(BuiltinDimensionTypes.OVERWORLD));
-		register(context, SNOWY, 1, Tags.Biomes.IS_SNOWY);
-		register(context, SOUL_SAND_VALLEY, 1, Biomes.SOUL_SAND_VALLEY);
-		register(context, STRAY, 2, new SlabfishEventCondition(Event.BREED), new SlabfishBreedCondition(SKELETON.location(), SKELETON.location()), biome(context, Tags.Biomes.IS_SNOWY));
 		register(context, TAIGA, 0, BiomeTags.IS_TAIGA);
-		register(context, TOTEM, 2, spawn(), new SlabfishRaidCondition());
-		register(context, WARM_OCEAN, 1, Biomes.WARM_OCEAN);
-		register(context, WARPED, 1, Biomes.WARPED_FOREST);
-		register(context, WITHER, 2, new SlabfishEventCondition(Event.BREED), new SlabfishBreedCondition(SKELETON.location(), SKELETON.location()), dimension(BuiltinDimensionTypes.NETHER));
 
-		register(context, RAINFOREST, 1, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalBiomeTags.IS_RAINFOREST);
+		register(context, WARM_OCEAN, 1, Biomes.WARM_OCEAN);
+		register(context, BAMBOO, 1, Biomes.BAMBOO_JUNGLE);
+		register(context, CHERRY_GROVE, 1, Biomes.CHERRY_GROVE);
+		register(context, DARK_FOREST, 1, Biomes.DARK_FOREST);
+		register(context, FLOWER_FOREST, 1, Biomes.FLOWER_FOREST);
+		register(context, ICE_SPIKES, 1, Biomes.ICE_SPIKES);
+		register(context, MANGROVE, 1, Biomes.MANGROVE_SWAMP);
+		register(context, MOUNTAIN, 1, spawn(), or(biome(context, Biomes.MEADOW), biome(context, Biomes.STONY_PEAKS)));
+		register(context, SNOWY, 1, Tags.Biomes.IS_SNOWY);
+
+		register(context, FROZEN_OCEAN, 2, spawn(), or(biome(context, Biomes.FROZEN_OCEAN), biome(context, Biomes.DEEP_FROZEN_OCEAN)));
+		register(context, MUSHROOM, 2, or(and(spawn(), biome(context, Biomes.MUSHROOM_FIELDS)), and(new SlabfishEventCondition(Event.LIGHTNING), new SlabfishTypeCondition(BROWN_MUSHROOM.location()))));
+		register(context, BROWN_MUSHROOM, 2, new SlabfishEventCondition(Event.LIGHTNING), new SlabfishTypeCondition(MUSHROOM.location()));
+
+		register(context, TOTEM, 3, spawn(), new SlabfishRaidCondition());
+		register(context, NIGHTMARE, 3, spawn(), new SlabfishInsomniaCondition(), new SlabfishTimeCondition(Time.NIGHT));
+
+		register(context, CAVE, 4, spawn(), new SlabfishHeightCondition(0, 48), new SlabfishLightCondition(0, 0, LightLayer.SKY), overworld());
+		register(context, DEEPSLATE, 4, spawn(), new SlabfishHeightCondition(Integer.MIN_VALUE, 0), new SlabfishLightCondition(0, 0, LightLayer.SKY), overworld());
+		register(context, SKY, 4, spawn(), new SlabfishHeightCondition(256, Integer.MAX_VALUE), overworld());
+
+		register(context, LUSH_CAVES, 5, Biomes.LUSH_CAVES);
+		register(context, DRIPSTONE_CAVES, 5, Biomes.DRIPSTONE_CAVES);
+		register(context, DEEP_DARK, 5, Biomes.DEEP_DARK);
+
+		register(context, DROWNED, 10, spawn(), biome(context, BiomeTags.IS_OCEAN), new SlabfishHeightCondition(Integer.MIN_VALUE, 48), new SlabfishInFluidCondition(context.lookup(Registries.FLUID).getOrThrow(FluidTags.WATER)));
+		register(context, SKELETON, 10, new SlabfishEventCondition(Event.LIGHTNING));
+		register(context, STRAY, 10, new SlabfishEventCondition(Event.BREED), new SlabfishBreedCondition(SKELETON.location(), SKELETON.location()), biome(context, Tags.Biomes.IS_SNOWY));
+		register(context, WITHER, 10, new SlabfishEventCondition(Event.BREED), new SlabfishBreedCondition(SKELETON.location(), SKELETON.location()), dimension(BuiltinDimensionTypes.NETHER));
+
+		register(context, BLOSSOM, 1, spawn(), or(biome(context, EnvironmentalBiomes.BLOSSOM_WOODS), biome(context, EnvironmentalBiomes.BLOSSOM_VALLEYS)));
+		register(context, MARSH, 1, EnvironmentalBiomes.MARSH);
+		register(context, PINE, 1, spawn(), or(biome(context, EnvironmentalBiomeTags.IS_PINE_BARRENS), biome(context, EnvironmentalBiomes.PINE_SLOPES)));
+
 		register(context, DUNES, 0, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalBiomeTags.IS_DUNES);
+		register(context, RAINFOREST, 1, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalBiomeTags.IS_RAINFOREST);
 		register(context, ASPEN, 1, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalConstants.ASPEN_PARKLAND);
-		register(context, KOUSA, 2, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalConstants.KOUSA_JUNGLE);
 		register(context, LAUREL, 1, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalConstants.LAUREL_FOREST);
 		register(context, SPINY_THICKET, 1, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalConstants.SPINY_THICKET);
+		register(context, KOUSA, 2, EnvironmentalConstants.ATMOSPHERIC, EnvironmentalConstants.KOUSA_JUNGLE);
 		register(context, SCRUBLAND, 2, modLoaded(EnvironmentalConstants.ATMOSPHERIC), spawn(), or(biome(EnvironmentalConstants.SCRUBLAND), biome(EnvironmentalConstants.SNOWY_SCRUBLAND)));
 
 		register(context, MAPLE, 1, modLoaded(EnvironmentalConstants.AUTUMNITY), spawn(), or(biome(EnvironmentalConstants.MAPLE_FOREST), biome(EnvironmentalConstants.PUMPKIN_FIELDS)));
 
+		register(context, NETHER, 0, spawn(), dimension(BuiltinDimensionTypes.NETHER));
+		register(context, CRIMSON, 1, Biomes.CRIMSON_FOREST);
+		register(context, WARPED, 1, Biomes.WARPED_FOREST);
+		register(context, SOUL_SAND_VALLEY, 1, Biomes.SOUL_SAND_VALLEY);
+		register(context, BASALT_DELTAS, 1, Biomes.BASALT_DELTAS);
+		register(context, GHOST, 0, new SlabfishImpossibleCondition());
+
+		register(context, END, 0, spawn(), dimension(BuiltinDimensionTypes.END));
+		register(context, CHORUS, 1, BlueprintBiomeTags.IS_OUTER_END);
 		register(context, POISE, 1, EnvironmentalConstants.ENDERGETIC, EnvironmentalConstants.POISE_FOREST);
 	}
 
@@ -217,6 +225,10 @@ public class EnvironmentalSlabfishTypes {
 
 	public static SlabfishDimensionCondition dimension(ResourceKey<DimensionType> dimension) {
 		return new SlabfishDimensionCondition(dimension.location());
+	}
+
+	public static SlabfishDimensionCondition overworld() {
+		return dimension(BuiltinDimensionTypes.OVERWORLD);
 	}
 
 	public static SlabfishModLoadedCondition modLoaded(String modid) {
