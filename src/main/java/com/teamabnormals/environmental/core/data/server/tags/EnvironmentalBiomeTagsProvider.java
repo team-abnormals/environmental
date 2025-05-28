1878ebd728bb8efea143d2ee13a5c67e78cb8774
0Navis0 <georges.alexis@outlook.com>
2025-05-28 08:47:12 +0200

    Update biome modifier

--- src/main/java/com/teamabnormals/environmental/core/data/server/tags/EnvironmentalBiomeTagsProvider.java
+++ src/main/java/com/teamabnormals/environmental/core/data/server/tags/EnvironmentalBiomeTagsProvider.java
@@ -104,7 +104,7 @@ public class EnvironmentalBiomeTagsProvider extends BiomeTagsProvider {
 
 		this.tag(EnvironmentalBiomeTags.HAS_CATTAILS).add(Biomes.RIVER, Biomes.SWAMP, Biomes.MANGROVE_SWAMP);
 		this.tag(EnvironmentalBiomeTags.HAS_CUP_LICHEN).addTag(BiomeTags.IS_TAIGA);
-		this.tag(EnvironmentalBiomeTags.HAS_DWARF_SPRUCE).addTag(BiomeTags.IS_TAIGA);
+		this.tag(EnvironmentalBiomeTags.HAS_SPARSE_DWARF_SPRUCE).addTag(BiomeTags.IS_TAIGA);
 		this.tag(EnvironmentalBiomeTags.HAS_BLUEBELL).add(Biomes.DARK_FOREST);
 		this.tag(EnvironmentalBiomeTags.HAS_VIOLET).addTag(BiomeTags.IS_TAIGA);
 		this.tag(EnvironmentalBiomeTags.HAS_TASSELFLOWER).addTag(BiomeTags.IS_SAVANNA);
--- src/main/java/com/teamabnormals/environmental/core/other/tags/EnvironmentalBiomeTags.java
+++ src/main/java/com/teamabnormals/environmental/core/other/tags/EnvironmentalBiomeTags.java
@@ -34,7 +34,7 @@ public class EnvironmentalBiomeTags {
 
 	public static final TagKey<Biome> HAS_CATTAILS = biomeTag("has_feature/cattails");
 	public static final TagKey<Biome> HAS_CUP_LICHEN = biomeTag("has_feature/cup_lichen");
-	public static final TagKey<Biome> HAS_DWARF_SPRUCE_SPARSE = biomeTag("has_feature/dwarf_spruce_sparse");
+	public static final TagKey<Biome> HAS_SPARSE_DWARF_SPRUCE = biomeTag("has_feature/dwarf_spruce_sparse");
 	public static final TagKey<Biome> HAS_BLUEBELL = biomeTag("has_feature/bluebell");
 	public static final TagKey<Biome> HAS_VIOLET = biomeTag("has_feature/violet");
 	public static final TagKey<Biome> HAS_TASSELFLOWER = biomeTag("has_feature/tasselflower");
